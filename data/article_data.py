- Added sales and stock computation - Fixed type of demand

data/article_data.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-import pandas as pd
+import numpy as np
 
 
 @dataclass
 class ArticleData:
     name: str
-    demand: pd.DataFrame
+    demand: np.array
     black_price: float
     stock: int
     salvage_value: float

data/demand_generator.py
@@ -7,7 +7,7 @@ DEFAULT_WEEKS_NUMBER = 30
 
 def create_random_demand(
     discount_grid=DEFAULT_DISCOUNTS_GRID, max_demand=1.0, seed=12345
-):
+) -> np.array:
     return (
         np.random.rand(len(DEFAULT_DISCOUNTS_GRID), DEFAULT_WEEKS_NUMBER)
         * max_demand
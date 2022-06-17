b3d936fd50d45b3af7df295b329dc06dcb404a22
nonvisual <nonvisual.wrk@gmail.com>
2022-06-17 17:22:55 +0200

    - Added demand generation with elasticities

--- data/demand_generator.py
+++ data/demand_generator.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import numpy as np
 import itertools
 
@@ -6,9 +7,18 @@ DEFAULT_WEEKS_NUMBER = 30
 
 
 def create_random_demand(
-    discount_grid=DEFAULT_DISCOUNTS_GRID, max_demand=1.0, seed=12345
+    discount_grid: np.array = DEFAULT_DISCOUNTS_GRID,
+    max_demand: float = 1.0,
+    elasticity=1.2,
+    seed: int = 12345,
 ) -> np.array:
-    return (
-        np.random.rand(len(DEFAULT_DISCOUNTS_GRID), DEFAULT_WEEKS_NUMBER)
-        * max_demand
+    # introduce elasticity concept = now it is entirely random
+    temp_demand = base_demand = (
+        np.random.rand(DEFAULT_WEEKS_NUMBER) * max_demand
     )
+    demand_result = base_demand
+    for i in range(1, len(DEFAULT_DISCOUNTS_GRID)):
+        delta_price = DEFAULT_DISCOUNTS_GRID[i] - DEFAULT_DISCOUNTS_GRID[i - 1]
+        temp_demand = temp_demand / (1 - elasticity * delta_price)
+        demand_result = np.vstack([demand_result, temp_demand])
+    return demand_result

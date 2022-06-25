9742fa319c2e5ac6428eeeecfbabfb080d0c2005
nonvisual <nonvisual.wrk@gmail.com>
2022-06-25 02:15:20 +0200

    - Extended kpi calculator to compute profit and left value

--- evaluation/kpi_calculator.py
+++ evaluation/kpi_calculator.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 from data.article_data import ArticleData
+from data.demand_generator import DEFAULT_DISCOUNTS_GRID
+from model.simple_model_creator import VAT
 
 
 def calculate(discounts: np.array, article_data: ArticleData):
@@ -13,10 +15,17 @@ def calculate(discounts: np.array, article_data: ArticleData):
     selected_demand = [article_data.demand[i] for i in choices]
     weekly_sales = []
     weekly_stock = []
+    weekly_profit = []
     current_stock = article_data.stock
     for i in range(len(selected_demand)):
         weekly_sales.append(min(current_stock, selected_demand[i]))
         current_stock = max(0, current_stock - weekly_sales[-1])
         weekly_stock.append(current_stock)
-
-    return weekly_sales, weekly_stock
+        weekly_profit.append(
+            article_data.black_price
+            * weekly_sales[-1]
+            * (1 - DEFAULT_DISCOUNTS_GRID[choices[i][0]])
+            / (1 + VAT)
+        )
+    left_value = weekly_stock[-1] * article_data.salvage_value
+    return weekly_sales, weekly_stock, weekly_profit, left_value

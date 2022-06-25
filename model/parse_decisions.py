4012c6154079c8753cc16a9ac2bf615528f51be3
nonvisual <nonvisual.wrk@gmail.com>
2022-06-25 02:15:51 +0200

    - Rewrote discounts parsing (bug)

--- model/parse_decisions.py
+++ model/parse_decisions.py
@@ -5,23 +5,33 @@ import numpy as np
 from data.demand_generator import DEFAULT_DISCOUNTS_GRID
 
 
-def parse_discounts(decision_vars, model):
+def parse_discounts(decision_vars, model, article_data):
     if model.status != pulp.const.LpStatusOptimal:
         raise Exception(f"Cannot parse solution of not optimally solved model")
-    indices = set([(i, w) for (i, d, w) in decision_vars.keys() if d == 0])
+
+    articles_num = len(article_data)
+    weeks_num = article_data[0].demand.shape[1]
+    levels_num = article_data[0].demand.shape[0]
+    indices = [(i, w) for i in range(articles_num) for w in range(weeks_num)]
 
     logging.info(f"Status is optimal, parsing solution")
+
     df = pd.DataFrame(
         [
-            d
-            for (i, d, w) in decision_vars.keys()
-            if decision_vars[i, d, w].varValue > 0.5
+            (
+                i,
+                w,
+                [
+                    d
+                    for d in range(levels_num)
+                    if decision_vars[i, d, w].varValue > 0.5
+                ][0],
+            )
+            for i in range(articles_num)
+            for w in range(weeks_num)
         ],
-        index=indices,
-        dtype=np.int64,
+        columns=["article", "week", "discount"],
     )
-    df = df.reset_index()
-    df.columns = ["article", "week", "discount"]
     df = df.sort_values(["article", "week"])
     objective = pulp.value(model.objective)
     return df, objective
